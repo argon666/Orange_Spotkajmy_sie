832792eedec2866117f9faac7b9d332ba9fba85c
argon666 <konrad.korczak18@gmail.com>
2021-04-10 14:28:26 +0200

    Add meetings sorting to Calendar class

--- AlgorithmLibrary/Calendar.cs
+++ AlgorithmLibrary/Calendar.cs
@@ -13,19 +13,32 @@ namespace AlgorithmLibrary
 
         public List<TimeInterval> Free_time(int min_free_minutes = 0)
         {
+            if(planned_meetings.Count==0)
+            {
+                return new List<TimeInterval>();
+            }
+            //Sorting planned meetings by start time
+            planned_meetings.Sort(); 
+
             List<TimeInterval> free_time = new();
-            if (planned_meetings[0].start.Subtract(working_hours.start).TotalMinutes > min_free_minutes) //work time start  to first meeting
+
+            //work time start  to first meeting
+            if (planned_meetings[0].start.Subtract(working_hours.start).TotalMinutes > min_free_minutes) 
             {
                 free_time.Add(new TimeInterval { start = working_hours.start, end = planned_meetings[0].start });
             }
-            for (int i = 0; i < planned_meetings.Count - 1; i++) // end of meeting "i" to start of meeting "i+1"
+
+            // end of meeting "i" to start of meeting "i+1"
+            for (int i = 0; i < planned_meetings.Count - 1; i++) 
             {
                 if (planned_meetings[i + 1].start.Subtract(planned_meetings[i].end).TotalMinutes > min_free_minutes)
                 {
                     free_time.Add(new TimeInterval { start = planned_meetings[i].end, end = planned_meetings[i + 1].start });
                 }
             }
-            if (working_hours.end.Subtract(planned_meetings[planned_meetings.Count - 1].end).TotalMinutes > min_free_minutes) // End of last meeting to end of work time
+
+            // End of last meeting to end of work time
+            if (working_hours.end.Subtract(planned_meetings[planned_meetings.Count - 1].end).TotalMinutes > min_free_minutes) 
             {
                 free_time.Add(new TimeInterval { start = planned_meetings[planned_meetings.Count - 1].end, end = working_hours.end });
             }
