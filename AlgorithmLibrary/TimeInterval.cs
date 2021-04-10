1f25fbae2a48267ebe4a482594d98912962162f7
argon666 <konrad.korczak18@gmail.com>
2021-04-10 14:25:51 +0200

    Add Icomparable<T> interface to TimeInterval

--- AlgorithmLibrary/TimeInterval.cs
+++ AlgorithmLibrary/TimeInterval.cs
@@ -2,21 +2,28 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace AlgorithmLibrary
 {
-    public class TimeInterval
+    
+    public class TimeInterval : IComparable<TimeInterval>
     {
         public DateTime start { get; set; }
         public DateTime end { get; set; }
         public override string ToString()
         {
-            return start.Hour + ":" + start.Minute + " " + end.Hour + ":" + (end.Minute);
+            return "[\"" + start.ToString("HH:mm") + "\",\"" + end.ToString("HH:mm") + "\"]";
         }
         public int Interval_in_minutes()
         {
             return (int)end.Subtract(start).TotalMinutes;
+        }
+        public int CompareTo(TimeInterval other)
+        {
+            return (int)start.Subtract(other.start).TotalMinutes;
+
         }
     }
 }
