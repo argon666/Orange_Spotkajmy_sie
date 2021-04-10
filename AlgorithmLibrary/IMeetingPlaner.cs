327b443a424b4b1e2344e3233ef61eab7dd7d2ec
argon666 <konrad.korczak18@gmail.com>
2021-04-10 14:26:52 +0200

    Create MeetingPlaner Interface

---
+++ AlgorithmLibrary/IMeetingPlaner.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace AlgorithmLibrary
+{
+    public interface IMeetingPlaner
+    {
+        List<TimeInterval> PlanMetting(ICalendar calendar1, ICalendar calendar2, int minDuration);
+    }
+}
